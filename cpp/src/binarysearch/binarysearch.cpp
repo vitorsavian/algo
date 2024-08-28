0fcbe2bb601c952527f76a7392af5a222cf5a85f
Vitor Savian <vitorandresaviandev@gmail.com>
2024-08-28 06:48:41 -0300

    Add binary search

---
+++ cpp/src/binarysearch/binarysearch.cpp
@@ -0,0 +1,5 @@
+
+
+int binarySearch() {
+  return 0;
+}
---
+++ cpp/src/binarysearch/binarysearch.h
@@ -0,0 +1,8 @@
+//
+// Created by Vitor Savian on 28/08/24.
+//
+
+#ifndef BINARYSEARCH_H
+#define BINARYSEARCH_H
+
+#endif //BINARYSEARCH_H
