ed7b9a2703c7c5cdaa8c99d2b94775e2b45d0c52
Vitor Savian <vitor.savian@suse.com>
2025-02-19 21:20:17 -0300

    ??: add a lot of more stuff
    Signed-off-by: Vitor Savian <vitor.savian@suse.com>

--- cpp/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <linked_list/linked_list.h>
-#include <binarysearch/binarysearch.h>
-
-int main() {
-    // TestLinkedList();
-    int size = 100;
-    Search(size);
-}
---
+++ go/pkg/array/search.go
@@ -0,0 +1,5 @@
+package array
+
+func binarySearch(arr []int) {
+
+}
