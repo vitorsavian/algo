26f6ed38107321476c7f890fd9ad653c28a9fcfa
Vitor Savian <vitorandresaviandev@gmail.com>
2025-06-01 17:25:53 -0300

    Add binary search and quicksort

--- lib/search/binary_search.cpp
+++ lib/search/binary_search.cpp
@@ -1,25 +1,26 @@
-// int binarySearch(int list[], int number, int low, int high) {
-//     if (list == nullptr) {
-//         return 0;
-//     }
+#include "binary_search.hpp"
+namespace search {
 
-//     int size = sizeof(list) / sizeof(list[0]);
+    template <typename T>
+    int BinarySearch(const T *arr, int size, const T &target) {
+        int left = 0;
+        int right = size - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+        return -1; // Target not found   
+    }
     
-//     if (high >= low) {
-//         int mid = low + (high - low) / 2;
-
-//         if (list[mid] == number) {
-//             return mid;
-//         }
-
-//         if (list[mid] > number) {
-
-//         }
-//     }
-    
-//     return number;
-
-// }
+}
 
 int main(int argc, const char** argv) {
     return 0;
--- lib/search/binary_search.hpp
+++ lib/search/binary_search.hpp
@@ -1,1 +1,12 @@
 #pragma once
+
+namespace search {
+    // Binary search algorithm implementation
+    // to actually use this function, the array must be sorted
+    // and the type T must support comparison operators (<, >, ==)
+    // You can use this function like search::BinarySearch(arr, size, target);
+    // or search::BinarySearch<int>(arr, size, target); for specific types.
+    template <typename T>
+    int BinarySearch(const T* arr, int size, const T& target);
+
+}
